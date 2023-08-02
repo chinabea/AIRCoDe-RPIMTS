81bdbb58c51fd66717df9f6b7348d0be3d511349
China T. Bea <134513410+ChichiBea@users.noreply.github.com>
2023-08-02 09:48:32 +0800

    commit

--- resources/views/submission-details/project-teams/edit.blade.php
+++
@@ -1,8 +0,0 @@
-<form action="{{ route('submission-details.project-teams.update', $projectTeam) }}" method="POST">
-    @csrf
-    @method('PUT')
-    <input type="text" name="member_name" value="{{ $projectTeam->member_name }}">
-    <input type="text" name="role" value="{{ $projectTeam->role }}">
-    <button type="submit">Update</button>
-</form>
-
