4dc3452e31aef07a7b7886d9d3742b47972717d9
China T. Bea <134513410+ChichiBea@users.noreply.github.com>
2023-08-31 09:31:14 +0800

    duplication removed

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -81,20 +81,10 @@
 <script src="{{ asset('datatables/datatables-demo.js') }}"></script>
 <!-- Summernote -->
 <script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
-
-<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
- <!-- Bootstrap 4 -->
- <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
- <!-- AdminLTE App -->
- <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
- <!-- Summernote -->
- <script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
  <!-- CodeMirror -->
  <script src="{{ asset('plugins/codemirror/codemirror.js') }}"></script>
  <script src="{{ asset('plugins/codemirror/mode/css/css.js') }}"></script>
  <script src="{{ asset('plugins/codemirror/mode/xml/xml.js') }}"></script>
  <script src="{{ asset('plugins/codemirror/mode/htmlmixed/htmlmixed.js') }}"></script>
- <!-- AdminLTE for demo purposes -->
- <script src="{{ asset('dist/js/demo.js') }}"></script>
 </body>
 </html>
